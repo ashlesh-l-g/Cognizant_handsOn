6407ca8b88c60b377c2476d0a2196d055a989ab3
Ashleshlg <ash4224.dev@gmail.com>
2026-07-01 00:09:16 +0530

    5. ReactJS-HOL

---
+++ cohorttracker/README.md
@@ -0,0 +1,15 @@
+# Cohort Tracker React Application - 5. ReactJS-HOL
+
+## Description
+
+This application displays cohort details and demonstrates styling in React using CSS Modules and inline styles.
+
+## Features
+
+- CSS Module styling
+- Dynamic status colors
+- Responsive cohort cards
+
+## Output Screenshot
+
+![Application Output](./screenshots/output.png)
---
+++ cohorttracker/src/CohortDetails.js
@@ -0,0 +1,34 @@
+import styles from './CohortDetails.module.css';
+
+function CohortDetails({ cohort }) {
+    return (
+        <div className={styles.box}>
+            <h3
+                style={{
+                    color:
+                        cohort.currentStatus === "ongoing"
+                            ? "green"
+                            : "blue"
+                }}
+            >
+                {cohort.currentStatus}
+            </h3>
+
+            <dl>
+                <dt>Started On</dt>
+                <dd>{cohort.startDate}</dd>
+
+                <dt>Current Status</dt>
+                <dd>{cohort.currentStatus}</dd>
+
+                <dt>Coach</dt>
+                <dd>{cohort.coachName}</dd>
+
+                <dt>Trainer</dt>
+                <dd>{cohort.trainerName}</dd>
+            </dl>
+        </div>
+    );
+}
+
+export default CohortDetails;
